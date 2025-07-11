adaafe66f3694b971398ea3b0f3672f790013e48
lectra-draconeey <77972170+lectra-draconeey@users.noreply.github.com>
2025-07-11 11:01:27 +0800

    This works, but have to account for the delay of actual hardware

--- PID_pi/old_controls.ino
+++ PID_pi/old_controls.ino
@@ -1,18 +1,22 @@
+/*
+  The following treats the Arduino as a mere dumb motor driver slave that simply complies with the commands received from the Pi
+  The command handling has been kept pretty simple for testing purposes to get down the transmission and systems integration
+  The previous code had a useless LCD attached that increased the command processing latency to 30 seconds because of lcd.print
+
+  The camera needs to ping and send details once every 30 frames only, otherwise the hardware gets overwhelmed with the barrage of commands and we make no progress.
+
+  ======================= The PID needs to be tuned more before it's usable, it's speed corrections are horrendous rn =======================
+*/
 #include <AFMotor.h>
 #include <Servo.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Common I2C address: 0x27
 
 Servo cameraServo;
 
-int cameraAngle = 0;  // Start at center
-
+// Initial angles for servos -- pls forgive the naming scheme i was using the servos for a rotating camera before
+int cameraAngle = 0;
 
 // === Motors ===
 #define DRIVE_SPEED 230
-
 #define TURN_SPEED 180
 
 AF_DCMotor motor1(1);
@@ -23,58 +27,28 @@ AF_DCMotor motor4(4);
 // === State ===
 bool driving = false;
 bool forward = true;
-bool steeringLeft = false;
-bool steeringRight = false;
-
 bool turningInPlace = false;
 bool turnLeftInPlace = false;
 bool turnRightInPlace = false;
 
-int steerOffset = 0; // -64 (left) to +64 (right)
-
 unsigned long lastCommandTime = 0;
-const unsigned long COMMAND_TIMEOUT = 300;
-
-// === The active speed ===
+const unsigned long COMMAND_TIMEOUT = 500;  // ms
 
 void setup() {
   Serial.begin(115200);
   stopMotors();
-  cameraServo.attach(46); // Connect to pin 44
+  cameraServo.attach(46); //  servo connected to pin 46, pin 44 isn't working, 45 is spotty at best...
   cameraServo.write(cameraAngle);
-  lcd.init();           // Initialize LCD
-  lcd.backlight();      // Turn on backlight
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Robot Ready!");
-
 }
 
 void loop() {
-  while (Serial.available()) {
-    char cmd = Serial.read();
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    char cmd = input.charAt(0);
     handleCommand(cmd);
     lastCommandTime = millis();
   }
 
-  // === Ramp steering ===
-  if (steeringLeft) {
-    steerOffset -= 1;
-    if (steerOffset < -64) steerOffset = -64;
-  } else if (steeringRight) {
-    steerOffset += 1;
-    if (steerOffset > 64) steerOffset = 64;
-  } else {
-    if (steerOffset > 0) {
-      steerOffset -= 1;
-      if (steerOffset < 0) steerOffset = 0;
-    } else if (steerOffset < 0) {
-      steerOffset += 1;
-      if (steerOffset > 0) steerOffset = 0;
-    }
-  }
-
-  // === Drive ===
   if (turningInPlace) {
     applyInPlaceTurn();
   } else if (driving) {
@@ -86,88 +60,67 @@ void loop() {
   if (millis() - lastCommandTime > COMMAND_TIMEOUT) {
     driving = false;
     turningInPlace = false;
+    stopMotors();
   }
-
-  delay(10);
 }
 
 void handleCommand(char cmd) {
-  lcd.setCursor(0,0);
-  lcd.print("Navigating...");
-  lcd.setCursor(0, 1);
-  lcd.print("Cmd: ");
-  lcd.print(cmd);
-  lcd.print("    ");
-
   switch (cmd) {
     case 'W':
       driving = true;
       forward = true;
+      turningInPlace = false;
       break;
 
     case 'S':
       driving = true;
       forward = false;
+      turningInPlace = false;
       break;
 
     case 'A':
-      if (!driving) {
-        turningInPlace = true;
-        turnLeftInPlace = false;
-        turnRightInPlace = true;
-      } else {
-        steeringLeft = false;
-        steeringRight = true;
-      }
+      driving = false;
+      turningInPlace = true;
+      turnLeftInPlace = false;
+      turnRightInPlace = true;
       break;
 
     case 'D':
-      if (!driving) {
-        turningInPlace = true;
-        turnRightInPlace = false;
-        turnLeftInPlace = true;
-      } else {
-        steeringRight = false; 
-        steeringLeft = true;
-      }
+      driving = false;
+      turningInPlace = true;
+      turnRightInPlace = false;
+      turnLeftInPlace = true;
       break;
 
     case 'X':
       driving = false;
       turningInPlace = false;
-      steeringLeft = false;
-      steeringRight = false;
+      stopMotors();
       break;
 
-    case 'U':  // Tilt up
+    case 'U': // Camera Tilt Up
       cameraAngle += 30;
-      if (cameraAngle > 180) cameraAngle = 180;
+      if (cameraAngle > 180) cameraAngle = 180; // Constrain angle
       cameraServo.write(cameraAngle);
       Serial.print("Camera angle: "); Serial.println(cameraAngle);
       break;
 
-    case 'N':  // Tilt down
+    case 'N': // Camera Tilt Down
       cameraAngle -= 30;
-      if (cameraAngle < 0) cameraAngle = 0;
+      if (cameraAngle < 0) cameraAngle = 0; // Constrain angle
       cameraServo.write(cameraAngle);
       Serial.print("Camera angle: "); Serial.println(cameraAngle);
       break;  
-
   }
-  lcd.setCursor(0,0);
-  lcd.print("What next?");
-};
+}
 
 void applyDrive() {
-  int temp = (DRIVE_SPEED * steerOffset);
-  int steerEffect = temp / 128;
-  int leftSpeed = constrain(DRIVE_SPEED - steerEffect, 0, 255);
-  int rightSpeed = constrain(DRIVE_SPEED + steerEffect, 0, 255);
+  int speed = DRIVE_SPEED;
 
-  motor1.setSpeed(leftSpeed);
-  motor2.setSpeed(leftSpeed);
-  motor3.setSpeed(rightSpeed);
-  motor4.setSpeed(rightSpeed);
+  motor1.setSpeed(speed);
+  motor2.setSpeed(speed);
+  motor3.setSpeed(speed);
+  motor4.setSpeed(speed);
 
   if (forward) {
     motor1.run(FORWARD);
@@ -180,11 +133,6 @@ void applyDrive() {
     motor3.run(BACKWARD);
     motor4.run(BACKWARD);
   }
-
-  Serial.print("L: "); Serial.print(leftSpeed);
-  Serial.print(" | R: "); Serial.print(rightSpeed);
-  Serial.print(" | Offset: "); Serial.print(steerOffset);
-  Serial.print(" | Speed: "); Serial.println(DRIVE_SPEED);
 }
 
 void applyInPlaceTurn() {
@@ -206,9 +154,6 @@ void applyInPlaceTurn() {
     motor3.run(BACKWARD);
     motor4.run(BACKWARD);
   }
-
-  Serial.print("In-place turning | Speed: ");
-  Serial.println(TURN_SPEED);
 }
 
 void stopMotors() {
